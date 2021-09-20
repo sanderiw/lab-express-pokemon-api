bbe00cb9d5ca6a9d5bd6d3d5e83f5b797af60df2
Sander Iwase <sanderiw@gmail.com>
2021-09-20 17:22:49 -0300

    Done - Missing Bonus

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = 4000;
 
@@ -6,6 +7,55 @@ const PORT = 4000;
 const allPokemon = require("./data");
 
 const app = express();
+// Setting up server to understand json
+app.use(express.json());
+
+// Get all pokemons from database
+app.get("/pokemon", (req, res) => {
+    console.log("Listened GET in /pokemon");
+    return res.status(200).json(allPokemon);
+});
+
+// Get pokemon detail by id
+app.get("/pokemon/:id", (req, res) => {
+    console.log(`Listened GET in /pokemon/${req.params.id}`);
+    const foundPokemon = allPokemon.find((pokemon) => {
+        return pokemon.id.toString() === req.params.id;
+    });
+    console.log(foundPokemon);
+    if (foundPokemon) {
+        return res.status(200).json(foundPokemon);
+    }
+
+    return res.status(400).json({ msg: "Pokemon not found" });
+});
+
+// Get pokemons searching by name
+app.get("/search", (req, res) => {
+    console.log(Object.keys(req.query));
+    if (req.query.name) {
+        const searchResult = allPokemon.filter((pokemonObj) => {
+            return pokemonObj.name
+                .toLowerCase()
+                .includes(req.query.name.toLowerCase());
+        });
+        if (searchResult.length) {
+            return res.status(200).json({ searchResult });
+        }
+    } else if (req.query.type) {
+        const searchResult = allPokemon.filter((pokemonObj) => {
+            for (let type of pokemonObj.types) {
+                if (type.toLowerCase().includes(req.query.type.toLowerCase())) {
+                    return true;
+                }
+            }
+        });
+        if (searchResult.length) {
+            return res.status(200).json({ searchResult });
+        }
+    }
+    return res.status(404).json({ msg: "Search not found" });
+});
 
 // -- Define your route listeners here! --
 
